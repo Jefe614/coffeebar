f9aaa5ef8ee6bf28348f59bcc717fdf1788b4586
Jefe614 <jeffkimani32@gmail.com>
2025-05-11 12:22:46 +0300

    Update package.json and configuration files for Tailwind CSS integration

--- food-ordering-app/vite.config.js
+++ food-ordering-app/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    // tailwindcss(),
   ],  optimizeDeps: {
     include: ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
   },
